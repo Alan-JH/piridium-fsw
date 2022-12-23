c0b5d95ba814e5255f97285da4a95dceff0da566
Alan-JH <alanjhsu08@gmail.com>
2022-12-22 21:00:30 -0500

    rudimentary mcp3208 driver

---
+++ drivers/mcp3208.py
@@ -0,0 +1,24 @@
+# MCP3208 Driver
+
+import spidev
+import RPi.GPIO as gp
+
+class ADC():
+    def __init__(self):
+        self.spibus = spidev.SpiDev()
+        self.spibus.open(0, 0)
+        self.spibus.max_speed_hz = 500000
+        self.spibus.mode = 0b00
+        
+    def sample(self, channel):
+        """
+        Reads and returns the 12 bit reading of the specified ADC channel
+        :param channel: (int) ADC channel, from 0 to 7 inclusive
+        :return: (int) 12 bit reading
+        """
+        control = (0b11 << 3) | (channel & 0b111) # Start bit, single ended mode, then channel select bits
+        mosi = [control >> 2, (control & 0b11) << 6, 0x00] # See fig 6-1 in mcp3208 datasheet
+        miso = self.spibus.xfer2(mosi)
+        return ((miso[1] & 0xf) << 8) | miso[2]
+
+    
