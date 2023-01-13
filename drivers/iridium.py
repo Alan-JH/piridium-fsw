ee17a52ab38b2caeb87760e73064d8e7b320e845
Alan-JH <alanjhsu08@gmail.com>
2023-01-13 15:36:21 -0500

    finish cleaning up iridium driver

--- drivers/iridium.py
+++ drivers/iridium.py
@@ -48,15 +48,28 @@ class Iridium():
         self.radio.request("AT*F", 1)
         self.radio.serial.close()
 
+    def soft_reset(self):
+        """
+        Resets settings without a power cycle
+        """
+        self.request("ATZn", 1)
+
     def net_avail(self):
         """
         Uses GPIO to determine whether network is available
+        :return: (bool) whether or not network is available
         """
         return self.gpio.net_available()
         
     def sbd_status(self):
         """
         Calls AT+SBDS
+        SBDS return format: <MO flag>, <MOMSN>, <MT flag>, <MTMSN>
+        MO flag: (1/0) whether message in mobile originated buffer
+        MOMSN: sequence number that will be used in the next mobile originated SBD session
+        MT flag: (1/0) whether message in mobile terminated buffer
+        MTMSN: sequence number in the next mobile terminated SBD session, -1 if nothing in the MT buffer
+        :return: (list) SBD Status return
         """
         return [int(i) for i in self.process("AT+SBDS").split(",")]
 
@@ -64,6 +77,7 @@ class Iridium():
         """
         Checks buffer for existing messages
         Doesnt use request because we don't to decode as utf-8
+        :return: (list) raw list of bytes
         """
         self.write("AT+SBDRB")
         raw = self.serial.read(50)
@@ -84,6 +98,11 @@ class Iridium():
         checksum = sum(message) & 0xffff
         message.append(checksum >> 8)  # add checksum bytes
         message.append(checksum & 0xff)
+        # For SBDWB, input message byte length
+        # Once "READY" is read in, write each byte, then the two least significant checksum bytes, MSB first
+        # Final response: 0: success, 1: timeout (insufficient number of bytes transferred in 60 seconds)
+        # 2: Checksum does not match calculated checksum, 3: message length too long or short
+        # Keep messages 340 bytes or shorter for 9602N modem
         self.SBD_WB(length)  # Specify bytes to write
         time.sleep(1)  # 1 second to respond
         if self.read().find("READY") == -1:
@@ -106,7 +125,15 @@ class Iridium():
 
     def sbd_initiate_x(self):
         """
-        AT+SBDIX call
+        AT+SBDIX call, initiates an SBD session and transfers messages out of MO buffer and into MT buffer
+        SBDIX return format: <MO status>,<MOMSN>,<MT status>,<MTMSN>,<MT length>,<MT queued>
+        MO status: 0: no message to send, 1: successful send, 2: error while sending
+        MOMSN: sequence number for next MO transmission
+        MT status: 0: no message to receive, 1: successful receive, 2: error while receiving
+        MTMSN: sequence number for next MT receive
+        MT length: length in bytes of received message
+        MT queued: number of MT messages in GSS waiting to be transferred to ISU
+        :return: (list) SBDIX call result
         """
         return [int(i) for i in self.process("AT+SBDIX", timeout=60).split(",")]
 
@@ -118,10 +145,9 @@ class Iridium():
 
     def network_time(self):
         """
-        System time, GMT, retrieved from satellite network (used as a network check)
-        returns a 32 bit integer formatted in hex, with no leading zeros. Counts number of 90 millisecond intervals
+        Processed system time, GMT, retrieved from satellite network (used as a network check)
+        MSSTM returns a 32 bit integer formatted in hex, with no leading zeros. Counts number of 90 millisecond intervals
         that have elapsed since the epoch
-        Requests, reads, processes, and returns current system time retrieved from network
         :return: (datetime) current time (use str() to parse to string if needed)
         """
         raw = self.request("AT-MSSTM")
@@ -139,7 +165,7 @@ class Iridium():
     def geolocation(self):
         """
         Geolocation at time of last contact with iridium constellation
-        raw return format: <x>, <y>, <z>, <time_stamp>
+        MSGEO return format: <x>, <y>, <z>, <time_stamp>
         time_stamp uses same 32 bit format as MSSTM, and indicates when the geolocation was last updated
         Converts from cartesian to lat/long/alt
         :return: (tuple) lat, long, altitude, time (unix timestamp)
@@ -154,7 +180,8 @@ class Iridium():
     def register(self, location=None):
         """
         Performs a manual registration, consisting of attach and location update. No MO/MT messages transferred
-        Optional param location: format [+|-]DDMM.MMM,[+|-]dddmm.mmm
+        :param location: (str) Optional location param, format [+|-]DDMM.MMM,[+|-]dddmm.mmm
+        :return: (str) raw processed result
         """
         if location:
             return self.process("AT+SBDREG", "=" + location)
@@ -162,8 +189,8 @@ class Iridium():
 
     def check_signal_active(self):
         """
-        Returns strength of satellite connection, may take up to ten seconds if iridium is in satellite handoff
-        Actively check signal strength, for transmit/receive timing
+        Actively requests strength of satellite connection, may take up to ten seconds if iridium is in satellite handoff
+        :return: (int) CSQ from 0 (weakest) to 5 (strongest)
         """
         raw = self.request("AT+CSQ", 10)  
         if raw.find("CSQ:") == -1:
@@ -173,67 +200,12 @@ class Iridium():
     def check_signal_passive(self):
         """
         Passively check signal strength, for transmit/receive timing. By default updates every 40 seconds
+        :return: (int) last known CSQ from 0 (weakest) to 5 (strongest)
         """
         raw = self.request("AT+CSQF")  
         if raw.find("CSQF:") == -1:
             return 0
         return int(raw[raw.find("CSQF:") + 5: raw.find("CSQF:") + 6])
-
-        self.RING_ALERT = lambda b="": self.request(f"AT+SBDMTA{b}")
-        # Enable or disable ring indications for SBD Ring Alerts. When ring indication is enabled, ISU asserts RI
-        # line and issues the unsolicited result code SBDRING when an SBD ring alert is received Ring alerts can only
-        # be sent after the unit is registered :optional param b: set 1/0 enable/disable
-
-        self.SOFT_RST = lambda: self.request("ATZn", 1)
-        # Resets settings without power cycle
-
-        # Load message into mobile originated buffer. SBDWT uses text, SBDWB uses binary. 
-        self.SBD_WT = lambda message: self.request(f"AT+SBDWT={message}")
-        # For SBDWB, input message byte length
-        # Once "READY" is read in, write each byte, then the two least significant checksum bytes, MSB first
-        # Final response: 0: success, 1: timeout (insufficient number of bytes transferred in 60 seconds)
-        # 2: Checksum does not match calculated checksum, 3: message length too long or short
-        # Keep messages 340 bytes or shorter
-        self.SBD_WB = lambda length: self.write(f"AT+SBDWB={length}")
-        # Read message from mobile terminated buffer. SBDRT uses text, SBDRB uses binary. Only one message is
-        # contained in buffer at a time
-        self.SBD_RT = lambda: self.request("AT+SBDRT")
-
-        # Returns state of mobile originated and mobile terminated buffers
-        # SBDS return format: <MO flag>, <MOMSN>, <MT flag>, <MTMSN>
-        self.SBD_STATUS = lambda: self.request("AT+SBDS")  # beamcommunications 101-102
-        # SBDSX return format: <MO flag>, <MOMSN>, <MT Flag>, <MTMSN>, <RA flag>, <msg waiting>
-        self.SBD_STATUS_EX = lambda: self.request("AT+SBDSX")  # beamcommunications 103
-        # MO flag: (1/0) whether message in mobile originated buffer
-        # MOMSN: sequence number that will be used in the next mobile originated SBD session
-        # MT flag: (1/0) whether message in mobile terminated buffer
-        # MTMSN: sequence number in the next mobile terminated SBD session, -1 if nothing in the MT buffer
-        # RA flag: (1/0) whether an SBD ring alert has been received and needs to be answered
-        # msg waiting: how many SBD mobile terminated messages are queued at the gateway for collection by ISU
-
-        self.SBD_TIMEOUT = lambda t=None: self.request(f"AT+SBDST={t}") if t else self.request("AT+SBDST")
-        # Reads or sets session timeout settings, after which time ISU will stop trying to transmit/receive to GSS,
-        # in seconds. 0 means infinite timeout
-
-        self.SBD_TRANSFER_MOMT = lambda: self.request("AT+SBDTC")  # beamcommunications 104
-        # Transfers contents of mobile originated buffer to mobile terminated buffer, to test reading and writing to
-        # ISU without initiating SBD sessions with GSS/ESS returns response of the form "SBDTC: Outbound SBD copied
-        # to Inbound SBD: size = <size>" followed by "OK", where size is message length in bytes
-
-        # Transmits contents of mobile originated buffer to GSS, transfer oldest message in GSS queuefrom GSS to ISU
-        self.SBD_INITIATE = lambda: self.request("AT+SBDI", 60)  # beamcommunications 94-95
-        # Like SBDI but it always attempts SBD registration, consisting of attach and location update. a should be
-        # "A" if in response to SBD ring alert, otherwise unspecified. location is an optional param,
-        # format =[+|-]DDMM.MMM, [+|-]dddmm.mmm
-        self.SBD_INITIATE_EX = lambda a="", location="": self.request("AT+SBDIX" + a, 60) if len(location) == 0 \
-            else self.request("AT+SBDIX" + a + "=" + location)  # beamcommunications 95-96
-        # returns: <MO status>,<MOMSN>,<MT status>,<MTMSN>,<MT length>,<MT queued>
-        # MO status: 0: no message to send, 1: successful send, 2: error while sending
-        # MOMSN: sequence number for next MO transmission
-        # MT status: 0: no message to receive, 1: successful receive, 2: error while receiving
-        # MTMSN: sequence number for next MT receive
-        # MT length: length in bytes of received message
-        # MT queued: number of MT messages in GSS waiting to be transferred to ISU
 
     def serial_test(self) -> bool:
         """
@@ -246,25 +218,26 @@ class Iridium():
         result = self.request("AT", 1)  # Give Iridium one second to respond
         if result.find("OK") != -1:
             return True
-        raise IridiumError(details="Serial test failed")
+        return False
 
     def functional(self):
         """
         Tests Iridium by loading a message into one buffer, transferring to the other, and reading the message
         :return: (bool) buffers functional
         """
-        self.serial_test()
-        result = self.request("AT+SBDWT=test")
-        if result.find("OK") == -1:
-            return False
-        result = self.request("AT+SBDTC", 1)
-        if result.find("Outbound SBD Copied to Inbound SBD: size = 4") == -1:
-            return False
-        result = self.request("AT+SBDRT")
-        if result.find("test") == -1:
-            return False
-        self.write("AT+SBDD2")  # clear all buffers
-        return True
+        if self.serial_test():
+            result = self.request("AT+SBDWT=test")
+            if result.find("OK") == -1:
+                return False
+            result = self.request("AT+SBDTC", 1)
+            if result.find("Outbound SBD Copied to Inbound SBD: size = 4") == -1:
+                return False
+            result = self.request("AT+SBDRT")
+            if result.find("test") == -1:
+                return False
+            self.write("AT+SBDD2")  # clear all buffers
+            return True
+        return False
 
     def process(self, cmd, arg="", timeout=0.5):
         """
